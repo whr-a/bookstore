f2c500b5c5c1ad1996156a868e48714e06e1d351
whr-a <2472356642@qq.com>
2022-12-15 11:13:17 +0800

    faster

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 int main ()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     database database_;
     string index,op;
     int n,value;
